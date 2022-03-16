a39c42018c40a7e59d76b689eb47ded4b0089105
marquitobb <maca9608@gmail.com>
2022-03-15 21:09:57 -0600

    work in find contacts

---
+++ find_contacts.py
@@ -0,0 +1,46 @@
+"""
+We're going to make our own Contacts application! The application must perform two types of operations:
+
+add name, where  is a string denoting a contact name. This must store  as a new contact in the application.
+find partial, where  is a string denoting a partial name to search the application for. It must count the number of contacts starting with  and print the count on a new line.
+Given  sequential add and find operations, perform each operation in order.
+
+Example
+Operations are requested as follows:
+
+add ed
+add eddie
+add edward
+find ed
+add edwina
+find edw
+find a
+"""
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'contacts' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts 2D_STRING_ARRAY queries as parameter.
+#
+
+def contacts(queries):
+    # Write your code here
+    for i in queries:
+        if "hac" in i:
+            print(i)
+        elif "find" in i:
+            # print(i)
+            pass
+
+if __name__ == '__main__':
+    queries = ['add hack', 'add hackerrank', 'find hac', 'find hak']
+    result = contacts(queries)
