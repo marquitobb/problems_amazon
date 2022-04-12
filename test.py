e2212eb30b39f1474ad3942ada73799c98dcae26
Marco Antonio Cobian Avalos <marcoantoniocobianavalos@MacBook-Pro-de-Marco.local>
2022-04-11 22:22:28 -0500

    add test for evaluate functions

--- test.py
+++ test.py
@@ -2,7 +2,34 @@
 """
 # TODO: add test completly
 
+import pytest
 from unittest import TestCase
-# from pytest import pytest
 
+# import functions interviews
+from .find_contacts import contacts
+from .brackets import isBalanced
+from .median import runningMedian
 
+def test_find_contacts():
+    """
+    Create a test case for find_contacts
+    """
+    queries = ['add hack', 'add hackerrank', 'find hac', 'find hak']
+    result = contacts(queries)
+    print(result)
+    assert int(result) == 2
+
+def test_isBalanced():
+    data = "{][({(}]][[[{}]][[[())}[)(]([[[)][[))[}[]][()}))](]){}}})}[{]{}{((}]}{{)[{[){{)[]]}))]()]})))["
+    result_balance = isBalanced(data)
+    print(result_balance)
+    assert result_balance == "NO"
+
+def test_runningMedian():
+    """
+    Create a test case for runningMedian
+    """
+    a = [7, 3, 5, 2, 4, 6, 8]
+    result = runningMedian(a)
+    print(result)
+    assert result == 2
